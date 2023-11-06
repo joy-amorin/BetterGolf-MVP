7a05c01bbb5ea6d6d34d95ea969eca8e1a3ebd94
Darian Grabino <daruangra@gmail.com>
2023-11-06 15:00:03 -0300

    add PlayerId

--- GolfApi/Models/DTOs/ScorecardDTOs/ScorecardListGetDTO.cs
+++ GolfApi/Models/DTOs/ScorecardDTOs/ScorecardListGetDTO.cs
@@ -4,10 +4,12 @@ public class ScorecardListGetDTO
 {
     public int Id { get; set; }
     public int PlayingHandicap { get; set; }
+    public int Player { get; set; }
 
     public ScorecardListGetDTO(Scorecard scorecard)
     {
         Id = scorecard.Id;
         PlayingHandicap = scorecard.PlayingHandicap;
+        Player = scorecard.PlayerId;
     }
 }
