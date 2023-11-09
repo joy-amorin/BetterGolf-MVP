07fc01fbdad88a237ed6b235f3c0ebe18ba11109
Darian Grabino <daruangra@gmail.com>
2023-11-09 17:43:43 -0300

    TotalStrokes for scorecard

--- GolfApi/Models/DTOs/ScorecardDTOs/ScorecardListGetDTO.cs
+++ GolfApi/Models/DTOs/ScorecardDTOs/ScorecardListGetDTO.cs
@@ -5,11 +5,13 @@ public class ScorecardListGetDTO
     public int Id { get; set; }
     public int PlayingHandicap { get; set; }
     public int Player { get; set; }
+    public int TotalStrokes { get; set; }
 
     public ScorecardListGetDTO(Scorecard scorecard)
     {
         Id = scorecard.Id;
         PlayingHandicap = scorecard.PlayingHandicap;
         Player = scorecard.PlayerId;
+        TotalStrokes = scorecard.TotalStrokes;
     }
 }
