b48b10abc85eec9e4f7da5b9ba9fd10f972d6770
Bruno <37399964+brncd@users.noreply.github.com>
2023-11-12 19:47:27 +0000

    cleaning

--- GolfApi/Models/DTOs/CourseDTOs/CoursePostDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace GolfApi.Models.DTOs.CourseDTOs;
-
-public class CoursePostDTO
-{
-    public string Name { get; set; } = null!;
-    public int CourseSlope { get; set; }
-    public double CourseRating { get; set; }
-    public int Par { get; set; }
-}
